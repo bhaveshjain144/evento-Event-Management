483f884fd4cd92e06d2286b17495da3b3654f32c
bhaveshjain144 <bhaveshjain1520.com>
2024-04-24 12:20:02 +0530

    adding file

--- evento/settings.py
+++ evento/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zat53i)rwzcqp3h+1c^ybjxlkjee8@9!acrczo!1-j-!c2)a*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 
 # Application definition
